a85c845dd45fa1ce8bc5f0bc9c8c63495f6fc524
Samuel Kaiser <samuel.kaiser01@gmail.com>
2019-02-23 18:07:52 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const announcementElement = document.getElementById('announcement')
+const esIstDoElements = document.getElementsByClassName('esistdo')
+
+function update(subtitle, title) {
+  // smallDiv.innerHTML = subtitle
+  announcementElement.innerHTML = title
+  announcementElement.style.visibility = 'visible'
+}
+
+function countdown(daysLeft) {
+  if (daysLeft === 0) return 'HEUTE'
+  if (daysLeft === 1) return 'morgen'
+  return `in ${daysLeft} Tagen`
+}
+
+const makeBlocks = (...lines) => lines
+  .map(line => `<span class="block">${line}</span>`)
+  .join('\n')
+
+const today = new Date().getDay()
+const donnerstag = 4
+const millisecondsPerDay = 86400000
+
+if (today === donnerstag) {
+  // ES IST WIEDER DONNERSTAG
+  document.title = 'Es ist wieder Donnerstag!'
+  Array.from(esIstDoElements).forEach(el => el.style.visibility = 'visible')
+} else {
+  // BALD IST WIEDER DONNERSTAG
+  Array.from(esIstDoElements).forEach(el => el.style.display = 'none')
+  const daysTilDonnerstag = (donnerstag + 7 - today) % 7
+  const nextDonnerstag =
+    new Date(Date.now() + daysTilDonnerstag * millisecondsPerDay)
+  const dateString =
+    nextDonnerstag.toLocaleDateString('de-AT', {month: 'long', day: 'numeric' })
+  const plural = daysTilDonnerstag > 1
+
+  document.title = countdown(daysTilDonnerstag)
+
+  update(
+    `${daysTilDonnerstag} Tag${plural ? 'e' : ''} noch!`,
+    makeBlocks(
+      countdown(daysTilDonnerstag),
+      `Am ${dateString}`,
+      `ist wieder`,
+      `Donners&#8203;tag!`
+    )
+  )
+}
