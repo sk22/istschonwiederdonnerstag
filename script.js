65bbf92f4965fb183f59ae2076816d1ed343aff0
Samuel Kaiser <samuel.kaiser01@gmail.com>
2019-02-24 01:42:05 +0100

    Fix do display

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const today = new Date().getDay()
 const donnerstag = 4
 const millisecondsPerDay = 86400000
 
-if (today === donnerstag) {
+if (true || today === donnerstag) {
   // ES IST WIEDER DONNERSTAG
   document.title = 'Es ist wieder Donnerstag!'
   show(esIstDoElement)
